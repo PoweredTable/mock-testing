8e5f90cdefd9acaa80f5c87d96abea5f99e556e6
Lucas da Silveira <lu058294@intelbras.com.br>
2023-05-01 21:36:01 -0300

    refactor: translate test

--- numberFact.test.js
+++ numberFact.test.js
@@ -3,12 +3,13 @@ const { getNumberFact } = require('./index');
 
 jest.mock('axios');
 
-test('should return a random fact about the number 6', async () => {
+test('Deve retornar um fato aleatório sobre o número 6', async () => {
   const response = {
     data: '6 is the highest number on one end of a standard domino.',
   };
   axios.get.mockResolvedValue(response);
   const result = await getNumberFact(6);
+ 
   expect(axios.get).toHaveBeenCalledWith('http://numbersapi.com/6');
   expect(result).toEqual(response.data);
 });
